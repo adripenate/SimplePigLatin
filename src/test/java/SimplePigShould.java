ea561badde31171964a432ae15532385d2e13737
adriana <adrianapenate98@gmail.com>
2020-01-18 12:59:23 +0000

    Red test - not_pig_when_string_is_not_a_word

--- src/test/java/SimplePigShould.java
+++ src/test/java/SimplePigShould.java
@@ -8,6 +8,11 @@ public class SimplePigShould {
         assertThat(SimplePig.pigIt("")).isEqualTo("");
     }
 
+    @Test
+    public void not_pig_when_string_is_not_a_word() {
+        assertThat(SimplePig.pigIt("!")).isEqualTo("!");
+    }
+
     private static class SimplePig {
         public static String pigIt(String phrase) {
             return "";
