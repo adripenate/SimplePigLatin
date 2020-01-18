cc23c0fb2e047e9cc536a21923fc21a2f9fc2071
adriana <adrianapenate98@gmail.com>
2020-01-18 12:55:31 +0000

    Red test - not_pig_when_string_is_empty

---
+++ src/test/java/SimplePigShould.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimplePigShould {
+    @Test
+    public void not_pig_when_string_is_empty() {
+        assertThat(SimplePig.pigIt("")).isEqualTo("");
+    }
+
+    private static class SimplePig {
+        public static String pigIt(String phrase) {
+            return null;
+        }
+    }
+}
